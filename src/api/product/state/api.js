3cf2902de97d5d8610ffa88170637cfaed5dcf52
ElieOko <elieoko100@gmail.com>
2024-09-27 06:12:06 +0100

    feat:add method api

---
+++ src/api/product/state/api.js
@@ -0,0 +1,17 @@
+const router = require("express").Router();
+
+router.post("/",(req,res)=>{
+
+});
+
+router.get("/",(req,res)=>{
+    
+});
+
+router.put("/",(req,res)=>{
+    
+});
+
+router.delete("/",(req,res)=>{
+    
+});
